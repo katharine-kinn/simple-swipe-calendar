2084e4883fe1307dc650535c1997b8b37b9a0e19
Catherine <Catherine@Mac-mini-Catherine.local>
2014-03-31 13:27:56 +0400

    Initial Commit

---
+++ calendar/KGCalendarAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KGCalendarAppDelegate.h
+//  calendar
+//
+//  Created by Catherine on 31/03/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGCalendarAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ calendar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  calendar
+//
+//  Created by Catherine on 31/03/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KGCalendarAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KGCalendarAppDelegate class]));
+    }
+}
